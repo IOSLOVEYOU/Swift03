a10f1955b7dc9c22087b9780af7ccd0688c5c634
IOSLOVEYOU <lixy@lixydeMacBook-Pro.local>
2015-11-12 10:55:44 +0800

    。。。

--- Swift03/main.swift
+++ Swift03/main.swift
@@ -8,5 +8,41 @@
 
 import Foundation
 
-println("Hello, World!")
 
+//字典
+
+var dic = ["age": 16, "name":"tom"];
+//println(dic)
+
+let oldName = dic.updateValue("lixy", forKey:"name")
+
+//[dic .removeValueForKey("age")]
+
+for(key,value)in dic
+{
+   // println("\(key):\(value)")
+}
+
+var testDic = Dictionary<String, Int>()
+testDic["年龄"] = 16
+
+
+testDic = [:]
+//println(testDic)
+
+func addNum (a:Int,b:Int)->Int
+{
+    return a+b
+}
+
+var test :(Int,Int)->Int = addNum
+
+var test1 :(Int,Int)->Int = addNum
+
+println(test1(2,3))
+
+func test3 :(addNum (Int,Int)->Int,String:str1,Int:b)->String
+{
+
+    
+}
